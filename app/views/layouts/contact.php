9f07d6e04cca35fabcce3c24f8fa5b6b5590d33f
Anthony Robin <contact@anthony-robin.fr>
2016-04-09 23:44:07 +0200

    Improve contact form design

--- app/views/layouts/contact.php
+++ app/views/layouts/contact.php
@@ -5,10 +5,10 @@
 
   <div class="medium-6 columns">
     <form action="<?= Slim\Slim::getInstance()->urlFor('contact'); ?>" method="post">
-      <input type="text" name="name" />
-      <input type="email" name="email" />
-      <textarea name="message" class="autosize"></textarea>
-      <input type="submit" value="submit" />
+      <input type="text" name="name" placeholder="Votre nom (*)" />
+      <input type="email" name="email" placeholder="Votre email (*)" />
+      <textarea name="message" class="autosize" placeholder="Votre message (*)"></textarea>
+      <input type="submit" value="Envoyer" class="button" />
     </form>
   </div>
 </div>
