5d35acf207ce5b025be272a1bc92311a9466f786
Anthony Robin <contact@anthony-robin.fr>
2016-04-13 23:40:54 +0200

    Setup schema.org microdatas to articles

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="<?= $page->description; ?>">
+    <meta name="description" content="<?= strip_tags_and_truncate($page->description); ?>">
     <meta name="keywords" content="<?= $page->keywords; ?>">
 
     <!-- Social meta -->
@@ -64,6 +64,7 @@
       </header>
 
       <?= $content; ?>
+      <?php require_once partials_path . '/_microdatas.php'; ?>
 
       <div class="push"></div>
     </main>
