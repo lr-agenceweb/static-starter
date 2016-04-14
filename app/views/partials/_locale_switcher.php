8c07b0dd18d7c7aae56ac964022e97b02e74e713
Anthony Robin <contact@anthony-robin.fr>
2016-04-14 14:27:46 +0200

    Setup active locale in menu

--- app/views/partials/_locale_switcher.php
+++ app/views/partials/_locale_switcher.php
@@ -1,10 +1,10 @@
 <?php if(count(AVAILABLE_LOCALES) > 1): ?>
 <nav>
   <ul class="menu align-right">
-    <li>
+    <li class="<?= locale() == 'en' ? 'active' : '' ?>">
       <a href="<?= $page->slug['en']; ?>"> <?= \App\I18N::t('locales.en'); ?> </a>
     </li>
-    <li>
+    <li class="<?= locale() == 'fr' ? 'active' : '' ?>">
       <a href="<?= $page->slug['fr']; ?>"> <?= \App\I18N::t('locales.fr'); ?> </a>
     </li>
   </ul>
